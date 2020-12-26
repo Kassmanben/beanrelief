9f0eef10dc798fed53f77c39191cc1df4961d332
Kassmanben <kassmanben@gmail.com>
2020-12-26 15:37:00 -0700

    Added react frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          January - $710 remaining in the pool
+      </div>
+    );
+  }
+}
+
+export default App;
