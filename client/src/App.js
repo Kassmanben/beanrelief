cc1039220e0e2742d50959f39be0b2342b757d98
Kassmanben <kassmanben@gmail.com>
2020-12-27 01:37:40 -0700

    Version1.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,236 @@
-import React, { Component } from "react";
+import React from "react";
+import Modal from "react-modal";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          January - $710 remaining in the pool
-      </div>
-    );
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "350px",
+    boxShadow: "0 7px 30px -10px rgba(150, 170, 180, 0.5)",
+  },
+};
+
+Modal.setAppElement("#root");
+
+function App() {
+  var subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [flashMessage, setFlashMessage] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [nameError, setNameError] = React.useState("");
+  const [amount, setAmount] = React.useState(0);
+  const [amountError, setAmountError] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [emailError, setEmailError] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [messageError, setMessageError] = React.useState("");
+
+  function openModal() {
+    setIsOpen(true);
   }
+
+  function afterOpenModal() {
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function sendEmail() {
+    if (
+      name &&
+      amount &&
+      message &&
+      email &&
+      nameError === "" &&
+      amountError === "" &&
+      messageError === "" &&
+      emailError === ""
+    ) {
+      var emailBody = { name, amount, message, email };
+      axios
+        .post("/", {
+          emailBody: emailBody,
+        })
+        .then((res) => {
+          try {
+            if (res.status === 200) {
+              closeModal();
+              setFlashMessage(
+                "Aid request submitted! We will try to respond and coordinate the aid payment within 1-3 days"
+              );
+            } else {
+              closeModal();
+              setFlashMessage("Sorry, something went wrong. Please try again");
+            }
+          } catch (err) {
+            closeModal();
+            setFlashMessage("Sorry, something went wrong. Please try again");
+          }
+        });
+    } else {
+      if (name === "") {
+        setNameError("Name is required");
+      }
+      if (amount === 0 || amount === "") {
+        setAmountError("Amount is required");
+      }
+      if (email === "") {
+        setEmailError("Email is required");
+      }
+      if (message === "") {
+        setMessageError("Message is required");
+      }
+    }
+  }
+
+  function isValidByRegexPattern(text, pattern) {
+    return pattern.test(String(text).toLowerCase());
+  }
+
+  function whichCharactersAreInvalidByRegexPattern(text, pattern) {
+    var invalidChars = text.match(pattern);
+    if (invalidChars) {
+      return invalidChars.join("").replace(/(.)(?=.*\1)/g, "");
+    }
+    return "";
+  }
+
+  function onBlur(text, pattern, inverse, name) {
+    var value = "";
+    var error = "";
+    if (isValidByRegexPattern(text, pattern)) {
+      value = text;
+    } else {
+      if (text === "") {
+        error = name + " is required";
+      } else {
+        if (name === "Name" && name === "Message") {
+          error =
+            name +
+            " cannot contain the characters: " +
+            whichCharactersAreInvalidByRegexPattern(text, inverse);
+        } else if (name === "Amount") {
+          error = "Number must be less that 1000";
+        } else if (name === "Email") {
+          error = "Invalid email address";
+        }
+      }
+    }
+    if (name === "Name") {
+      setName(value);
+      setNameError(error);
+    } else if (name === "Amount") {
+      setAmount(value);
+      setAmountError(error);
+    } else if (name === "Email") {
+      setEmail(value);
+      setEmailError(error);
+    } else if (name === "Message") {
+      setMessage(value);
+      setMessageError(error);
+    }
+  }
+
+  return (
+    <div>
+      <div className="flash-message">{flashMessage}</div>
+      <h1>Bean Relief Fund</h1>
+      <h2>January - $710 remaining</h2>
+      <button onClick={openModal}>Request Aid</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Aid Description"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Aid Description</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name*</label>
+            <input
+              type="text"
+              name="name"
+              onFocus={() => setNameError("")}
+              onBlur={(e) => {
+                onBlur(
+                  e.target.value,
+                  /^[a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžßÇŒÆČŠŽ∂ð ,.'-]{1,250}$/iu,
+                  /[^a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžßÇŒÆČŠŽ∂ð\s,.'-]/giu,
+                  "Name"
+                );
+              }}
+            ></input>
+            <small className={nameError === "" ? "hidden" : "visible"}>
+              {nameError}
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="amount">Amount Requesting ($)*</label>
+            <input
+              type="number"
+              name="amount"
+              onFocus={() => setAmountError("")}
+              onBlur={(e) => {
+                onBlur(e.target.value, /^[\d]{1,3}$/iu, "", "Amount");
+              }}
+            ></input>
+            <small className={amountError === "" ? "hidden" : "visible"}>
+              {amountError}
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              onFocus={() => setEmailError("")}
+              onBlur={(e) => {
+                onBlur(
+                  e.target.value,
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/iu,
+                  "",
+                  "Email"
+                );
+              }}
+            ></input>
+            <small className={emailError === "" ? "hidden" : "visible"}>
+              {emailError}
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">What will this be used for?*</label>
+            <textarea
+              rows="7"
+              name="message"
+              onFocus={() => setMessageError("")}
+              onBlur={(e) => {
+                onBlur(
+                  e.target.value,
+                  /^[a-z\d\-_\s!"#$%&'()*+,-.:;<=>?@[\]^_~]+$/iu,
+                  /[^a-z\d\-_\s!"#$%&'()*+,-./:;<=>?@[\]^/_~]+$/i,
+                  "Message"
+                );
+              }}
+              placeholder="Describe the reasons that you are requesting aid. Ideally, all requests will be filled, but in the case that there are too many, we will try to prioritize medical needs, food, and rent assistance."
+            ></textarea>
+            <small className={messageError === "" ? "hidden" : "visible"}>
+              {messageError}
+            </small>
+          </div>
+        </form>
+        <button onClick={sendEmail}>Send</button>
+        <button onClick={closeModal}>Cancel</button>
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
--- index.js
+++ index.js
@@ -1,10 +1,71 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
+var mandrill = require("mandrill-api");
+
+dotenv.config({ path: "./config/config.env" });
+var mandrill_client = new mandrill.Mandrill(process.env.MANDRILL);
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
+
+// Put all API endpoints under '/api'
+app.post("/", (req, res) => {
+  console.log(req.body);
+  var text =
+    "<p><ul><li>Name: " +
+    req.body.emailBody.name +
+    "</li><li>Amount: " +
+    req.body.emailBody.amount +
+    "</li><li>Email: " +
+    req.body.emailBody.email +
+    "</li><li>Message: " +
+    req.body.emailBody.message +
+    "</li></ul></p>";
+  var message = {
+    html: text,
+    text: text
+      .replace("<p>", "")
+      .replace("</p>", "")
+      .replace("<ul>", "")
+      .replace("</ul>", "")
+      .replace("<li>", "")
+      .replace("</li>", ""),
+    subject: "Mutual Aid request",
+    from_email: "me@kassmanben.com",
+    from_name: req.body.emailBody.name,
+    to: [
+      {
+        email: "help@beanrelief.com",
+        name: "Aid",
+        type: "to",
+      },
+    ],
+  };
+  var async = false;
+  var ip_pool = "Main Pool";
+  mandrill_client.messages.send(
+    {
+      message: message,
+      async: async,
+      ip_pool: ip_pool,
+    },
+    () => {
+      res.json({ successMessage: "success" });
+    },
+    function (err) {
+      console.log(
+        "A mandrill error occurred: " + err.name + " - " + err.message
+      );
+    }
+  );
+});
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -12,7 +73,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Bean relief listening on ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`)
+);
